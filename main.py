7c57ff868d69ee97d687117839c213009144bb5b
Aswin <aswinss0018@gmail.com>
2025-09-06 16:57:32 +0530

    get api

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
-app=FastAPI()
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating:Optional[int]=None
 
 @app.get("/")
+def root():
+    return {"message": "Hello World!"}
 
-def get_user():
-    return {"message":"Hello World!"}
+@app.get("/posts")
+def get_posts():
+    return {"data": "this is your posts!"}
+
+@app.post("/createposts")
+def create_posts(new_post: Post):
+    return {
+        "message": "Successfully created post!",
+        "data": new_post,
+    
+    }
